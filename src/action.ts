60b8a564dedb6bd63e9f9263884a5a06bdb5abdc
Walter Manger <walter.manger@nrccua.org>
2020-12-17 16:38:10 -0700

    Getting the token

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,17 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {Context} from '@actions/github/lib/context'
 
-async function action(): Promise<void> {
+async function action(context: Context = github.context): Promise<void> {
   try {
+    const token = process.env.REPO_ACCESS_TOKEN!
+    const client = github.getOctokit(token)
+    core.debug(`client: ${JSON.stringify(client)}`)
+    //const configPath = core.getInput('configuration-path', {required: true})
+    //core.debug(`configPath: ${JSON.stringify(configPath)}`)
+
     core.setOutput('time', new Date().toTimeString())
+    core.setOutput('context', context)
   } catch (error) {
     core.setFailed(error.message)
   }
