d9c969e5896de385145a9faff1898b651f61532d
Walter Manger <walter.manger@nrccua.org>
2020-12-18 10:27:22 -0700

    ?

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,36 +1,109 @@
 import * as process from 'process'
-// import * as cp from 'child_process'
-// import * as path from 'path'
-// import action from '../src/action'
+import * as fs from 'fs'
+import * as path from 'path'
+// @ts-ignore
+import nock from 'nock'
+import action from '../src/action'
+import {Context} from '@actions/github/lib/context'
+import {WebhookPayload} from '@actions/github/lib/interfaces'
+
+nock.disableNetConnect()
 
 describe('pr-label-by-base', () => {
   beforeEach(() => {
     setupEnvironmentVariables()
   })
 
-  it('does almost nothing', async () => {})
+  it('adds the "main" label for "main" branch', async () => {
+    nock('https://api.github.com')
+      .get(
+        '/repos/Codertocat/Hello-World/contents/.github%2Fpr-label-by-base.yml?ref=main'
+      )
+      .reply(200, configFixture())
+      .post('/repos/Codertocat/Hello-World/issues/1/labels', (body: any) => {
+        expect(body).toMatchObject({
+          labels: ['main']
+        })
+        return true
+      })
+      .reply(200)
+
+    await action(
+      new MockContext(pullRequestOpenedFixture({ref: 'fix'}, {baseRef: 'main'}))
+    )
+    expect.assertions(1)
+  })
 })
 
-// test('throws invalid number', async () => {
-//   const input = parseInt('foo', 10)
-//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-// })
+class MockContext extends Context {
+  constructor(payload: WebhookPayload) {
+    super()
+    this.payload = payload
+  }
+}
 
-// shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
+function encodeContent(content: Buffer) {
+  return Buffer.from(content).toString('base64')
+}
+
+function configFixture(fileName = 'config.yml') {
+  return {
+    type: 'file',
+    encoding: 'base64',
+    size: 5362,
+    name: fileName,
+    path: `.github/${fileName}`,
+    content: encodeContent(
+      fs.readFileSync(path.join(__dirname, `fixtures/${fileName}`))
+    ),
+    sha: '3d21ec53a331a6f037a91c368710b99387d012c1',
+    url:
+      'https://api.github.com/repos/octokit/octokit.rb/contents/.github/release-drafter.yml',
+    git_url:
+      'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+    html_url:
+      'https://github.com/octokit/octokit.rb/blob/master/.github/release-drafter.yml',
+    download_url:
+      'https://raw.githubusercontent.com/octokit/octokit.rb/master/.github/release-drafter.yml',
+    _links: {
+      git:
+        'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+      self:
+        'https://api.github.com/repos/octokit/octokit.rb/contents/.github/release-drafter.yml',
+      html:
+        'https://github.com/octokit/octokit.rb/blob/master/.github/release-drafter.yml'
+    }
+  }
+}
+
+function pullRequestOpenedFixture(
+  {ref}: {ref: string},
+  {baseRef}: {baseRef: string}
+) {
+  return {
+    pull_request: {
+      number: 1,
+      head: {
+        ref
+      },
+      base: {
+        ref: baseRef
+      }
+    },
+    repository: {
+      name: 'Hello-World',
+      owner: {
+        login: 'Codertocat'
+      }
+    }
+  }
+}
 
 function setupEnvironmentVariables() {
   // configuration-path parameter is required
   // parameters are exposed as environment variables: https://help.github.com/en/github/automating-your-workflow-with-github-actions/workflow-syntax-for-github-actions#jobsjob_idstepswith
   process.env['GITHUB_REPOSITORY'] = ''
-  process.env['INPUT_CONFIGURATION-PATH'] = '.github/pr-labeler.yml'
+  process.env['INPUT_CONFIGURATION-PATH'] = '.github/pr-label-by-base.yml'
   process.env['REPO_ACCESS_TOKEN'] =
     "it doesn't matter here, but you need it for the real thing"
 }
--- src/action.ts
+++ src/action.ts
@@ -1,20 +1,107 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as yaml from 'js-yaml'
 import {Context} from '@actions/github/lib/context'
+import {GitHub} from '@actions/github/lib/utils'
+
+type PullRequestValidation = {
+  PullRequestNumber?: number
+  ErrorMessage?: string
+}
+
+interface RepoInfo {
+  owner: string
+  repo: string
+}
 
 async function action(context: Context = github.context): Promise<void> {
   try {
     const token = process.env.REPO_ACCESS_TOKEN!
     const client = github.getOctokit(token)
-    core.debug(`client: ${JSON.stringify(client)}`)
-    //const configPath = core.getInput('configuration-path', {required: true})
-    //core.debug(`configPath: ${JSON.stringify(configPath)}`)
+    const configPath = core.getInput('configuration-path', {required: true})
 
-    core.setOutput('time', new Date().toTimeString())
-    core.setOutput('context', context)
+    const prValidation = validatePullRequest(context)
+    if (prValidation.ErrorMessage) {
+      core.info(prValidation.ErrorMessage)
+      return
+    }
+
+    const targetBranch = context.payload?.pull_request?.base.ref
+    const labels: string[] = await getLabels(
+      client,
+      configPath,
+      targetBranch,
+      context
+    )
+    core.debug(`labels to apply: ${labels}`)
+
+    if (labels && labels.length > 0) {
+      await client.issues.addLabels({
+        ...context.repo,
+        issue_number: prValidation.PullRequestNumber!,
+        labels
+      })
+    }
   } catch (error) {
+    core.error(error)
     core.setFailed(error.message)
   }
+}
+
+function validatePullRequest(
+  context: Context = github.context
+): PullRequestValidation {
+  const requireOpenedAction = false
+
+  if (!context.payload.pull_request) {
+    return {
+      ErrorMessage:
+        'This action is supposed to run for pull requests only, exiting'
+    }
+  }
+
+  if (requireOpenedAction && context.payload.action !== 'opened') {
+    return {
+      ErrorMessage:
+        'This action is supposed to run for pull requests that are being opened only.'
+    }
+  }
+
+  return {PullRequestNumber: context.payload.pull_request.number}
+}
+
+async function getLabels(
+  client: InstanceType<typeof GitHub>,
+  configurationPath: string,
+  branchRef: string,
+  context: Context
+): Promise<string[]> {
+  const configurationContent: string = await fetchContent(
+    client,
+    configurationPath,
+    context.repo,
+    branchRef
+  )
+
+  const configObject: any = yaml.safeLoad(configurationContent)
+  core.debug(`labels for ${branchRef}: ${JSON.stringify(configObject)}`)
+  return configObject['branchLabels'][branchRef]
+}
+
+async function fetchContent(
+  client: InstanceType<typeof GitHub>,
+  path: string,
+  {owner, repo}: RepoInfo,
+  ref: string
+): Promise<string> {
+  const response: any = await client.repos.getContent({
+    owner,
+    repo,
+    path,
+    ref
+  })
+
+  return Buffer.from(response.data.content, response.data.encoding).toString()
 }
 
 export default action
